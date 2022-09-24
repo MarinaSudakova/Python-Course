d4fe79100cb9522a134313b91698b6cab5448277
Marina Sudakova <rubbish_bin@inbox.ru>
2022-09-24 10:56:15 +0500

    Task_2

---
+++ 1_Lecture/Task_2_print.py
@@ -0,0 +1,11 @@
+name = 'Кеша'
+age = 2
+print(name, age)
+
+# использование разделителя
+print(name, age, sep='+/-')
+
+# перенос строки
+print(name, end='\n')
+print(age, end=';')
+print('end', end='!')
